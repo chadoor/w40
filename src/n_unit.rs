09b3867efc06e9db38b5ea1d0ac000cfdb46d73e
chadoor <chaor32@gmail.com>
2024-03-26 22:53:46 +0300

    added n_unit file which inludes meta data about units.json

--- src/n_unit.rs
+++ src/n_unit.rs
@@ -1,5 +1,40 @@
-{
-    "kaballite_warriors": {
+use crate::weapon::*;
+
+struct Stats{
+    movement: u8,
+    toughness: u8,
+    armor_save: u8,
+    wounds: u8,
+    leadership: u8,
+    objective_control: u8,
+    invulnerable_save: u8
+}
+
+struct meta_unit{
+    unit_stats: Stats,
+    ranged_weapons: Vec<String>,
+    melee_weapons: Vec<String>,
+    keywords: Vec<String>,
+    faction_keywords: String,
+    abilities: Vec<String>,
+    faction_abilities: String,
+    core_abilities: Vec<String>,
+    unit_composition: (String,u8),
+    weapon_composition: Vec<String>
+}
+
+pub struct n_unit{
+    name: String,
+    unit_stats: Stats,
+    weapons: Vec<Weapon>,
+    keywords: Vec<String>,
+    abilities: Vec<String>,
+    models: u8
+}
+
+
+/*
+ "kaballite_warriors": {
         "unit_stats": [
             {
                 "movement": 8,
@@ -28,4 +63,6 @@
         "unit_composition": [{"Sybarite":1},{"Kaballite_Warrior":9}],
         "weapon_composition": ["splinter_rifle","ccw"]
     }
-}
+
+*/
+
